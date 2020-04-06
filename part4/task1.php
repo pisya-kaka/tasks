c1981b9a7009396c0f143794afc66e01f8bcafb9
user <user@user.user>
2020-04-06 15:03:57 +0700

    puki

--- part4/task1.php
+++ part4/task1.php
@@ -1,4 +1,9 @@
 <?php
+
+/*
+ * Дан массив с элементами 'html', 'css', 'php', 'js', 'jq'.
+ * С помощью цикла foreach выведите эти слова в столбик.
+ */
 $arr = ['html', 'css', 'php', 'js', 'jq'];
 foreach ($arr as $elem) {
     echo $elem.'<br>';
