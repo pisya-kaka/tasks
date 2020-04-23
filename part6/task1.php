92ef7c979582639551e41ee6be34079597501a2a
user <user@user.user>
2020-04-23 07:49:42 +0700

    task1

---
+++ part6/task1.php
@@ -0,0 +1,53 @@
+<?php
+/*
+ * Дана строка 'php'.
+ *  Сделайте из нее строку 'PHP'.
+ */
+
+    strtoupper ("php");
+    echo strtoupper("php")."<hr/>";
+
+
+/*
+ * Дана строка 'PHP'.
+ *  Сделайте из нее строку 'php'.
+ */
+    strtolower("PHP");
+    echo strtolower("PHP")."<hr/>";
+
+
+
+/*
+ * Дана строка 'london'.
+ *  Сделайте из нее строку 'London'.
+ */
+    ucfirst("london");
+    echo ucfirst("london")."<hr/>";
+
+
+
+/*
+ *Дана строка 'London'.
+ *Сделайте из нее строку 'london'.
+ */
+    lcfirst("London");
+    echo lcfirst("London")."<hr/>";
+
+
+
+/*
+ * Дана строка 'london is the capital of great britain'.
+ *Сделайте из нее строку 'London Is The Capital Of Great Britain'.
+ */
+    ucwords('london is the capital of great britain');
+    echo ucwords('london is the capital of great britain')."<hr/>";
+
+
+
+/*
+ * Дана строка 'LONDON'.
+ *  Сделайте из нее строку 'London'.
+ */
+
+    strtolower("LONDON");
+    echo strtolower("LONDON")."<hr/>";
